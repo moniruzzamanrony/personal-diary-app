ed3e24d72be383b05eea88d60be9781a5d4fe9bd
Moniruzzaman Rony <eproni29@gmail.com>
2021-11-10 00:46:18 +0600

    FEAT: ADDED LOGIN AND SIGNUP PAGE UI

--- personal-diary-app-ui/src/app/main/features/users/users-routing.module.ts
+++ personal-diary-app-ui/src/app/main/features/users/users-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {LoginComponent} from "./login/login.component";
+import {SignUpComponent} from "./sign-up/sign-up.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'sign-up', component: SignUpComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class UsersRoutingModule { }
+export class UsersRoutingModule {
+}
